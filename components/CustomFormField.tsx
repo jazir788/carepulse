3ffade0929e844764393de2d733244d0a772fd7e
jazir788 <jazirsx5@gmail.com>
2024-11-11 19:26:10 +0000

    added datepicker and skeleton logic to customformfiled component

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import {
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
@@ -15,6 +14,9 @@ import { FormFieldType } from './forms/PatientForm';
 import Image from 'next/image';
 import PhoneInput from 'react-phone-number-input';
 import 'react-phone-number-input/style.css'
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
   interface CustomProps {
     control: Control<any>,
@@ -34,7 +36,7 @@ import 'react-phone-number-input/style.css'
 
 
 const RenderField = ({field, props}: {field:any, props: CustomProps}) => {
-    const {fieldType, iconSrc, iconAlt, placeholder}  = props
+    const {fieldType, iconSrc, iconAlt, placeholder, showTimeSelect, dateFormat, renderSkeleton}  = props
 
     switch(fieldType){
         case FormFieldType.INPUT:
@@ -71,6 +73,35 @@ const RenderField = ({field, props}: {field:any, props: CustomProps}) => {
             </FormControl>
 
             )
+        case FormFieldType.DATE_PICKER:
+          return(
+          <div className='flex rounded-md border border-dark-500 bg-dark-400 '>
+            <Image
+              src="/assets/icons/calendar.svg"
+              height={24}
+              width={24}
+              alt='calendar'
+              className='ml-2' 
+            />
+            <FormControl>
+              <DatePicker 
+                selected={field.value} 
+                onChange={(date) => field.onChange(date)}
+                dateFormat= {dateFormat ?? 'MM/dd/yyyy' }
+                showTimeSelect={showTimeSelect ?? false}
+                timeInputLabel='Time:'
+                wrapperClassName='dae-picker'
+              />
+
+            </FormControl>
+          </div>
+          )
+
+          case FormFieldType.SKELETON:
+            return(
+              renderSkeleton ? renderSkeleton(field) : null
+            )
+
             default:
                 break;
 
