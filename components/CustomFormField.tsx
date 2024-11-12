a257c4103b09c2c52c13a828dcd430297c043b44
jazir788 <jazirsx5@gmail.com>
2024-11-12 19:22:03 +0000

    Added select component into customformfield component

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -17,6 +17,8 @@ import 'react-phone-number-input/style.css'
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+import { Select, SelectContent, SelectTrigger, SelectValue } from './ui/select';
+
 
   interface CustomProps {
     control: Control<any>,
@@ -96,6 +98,22 @@ const RenderField = ({field, props}: {field:any, props: CustomProps}) => {
             </FormControl>
           </div>
           )
+          case FormFieldType.SELECT:
+            return (
+              <FormControl>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger className="shad-select-trigger">
+                      <SelectValue placeholder={placeholder} />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent className="shad-select-content">
+                    {props.children}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+            );
+
 
           case FormFieldType.SKELETON:
             return(
